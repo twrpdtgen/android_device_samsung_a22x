ebcc643b36b7d4593fbd33f5787b8dd908850e1e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-05 21:45:02 +0200

    a22x: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A226B Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a22x.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a22x-user \
+    omni_a22x-userdebug \
+    omni_a22x-eng
